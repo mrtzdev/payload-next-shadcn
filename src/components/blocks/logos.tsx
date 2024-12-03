01bb76df583d1b059863bdb442aa728d9e3ee2fd
Moritz R <mrtz.dev@gmail.com>
2024-12-03 10:56:11 +0100

    change logos block

--- src/components/blocks/logos.tsx
+++ src/components/blocks/logos.tsx
@@ -1,10 +1,10 @@
-import { LogosBlock } from "@/payload-types";
-import Image from "next/image";
+import { LogosBlock } from '@/payload-types'
+import Image from 'next/image'
 
-const imageUrl = `${process.env.NEXT_PUBLIC_PAYLOAD_SERVER_URL}`;
+const imageUrl = `${process.env.NEXT_PUBLIC_PAYLOAD_SERVER_URL}`
 
 export default function Logos(props: LogosBlock) {
-  const { logosHeading, logosText, logos } = props;
+  const { logosHeading, logosText, logos } = props
   return (
     <>
       <section className="py-20">
@@ -15,9 +15,7 @@ export default function Logos(props: LogosBlock) {
             </h3>
           )}
           {logosText && (
-            <p className="mb-8 max-w-3xl text-muted-foreground lg:text-lg">
-              {logosText}
-            </p>
+            <p className="mb-8 max-w-3xl text-muted-foreground lg:text-lg">{logosText}</p>
           )}
         </div>
         <div className="pt-5 md:pt-10 lg:pt-10">
@@ -28,15 +26,15 @@ export default function Logos(props: LogosBlock) {
                 className="basis-1/3 pl-0 sm:basis-1/4 md:basis-1/5 lg:basis-1/6 my-5"
               >
                 <div className="mx-12 flex items-center justify-center">
-                  {typeof item.logo === "object" && (
+                  {typeof item.logo === 'object' && (
                     <>
                       {item.logo?.url && (
                         <Image
                           src={imageUrl + item.logo?.url}
                           width={item.logo?.width ? item.logo?.width : 200}
                           height={item.logo?.height ? item.logo?.height : 200}
-                          alt={item.logo?.alt ? item.logo?.alt : "Logo"}
-                          className="w-28 my-auto"
+                          alt={item.logo?.alt ? item.logo?.alt : 'Logo'}
+                          className="w-40 my-auto"
                         />
                       )}
                     </>
@@ -48,5 +46,5 @@ export default function Logos(props: LogosBlock) {
         </div>
       </section>
     </>
-  );
+  )
 }
